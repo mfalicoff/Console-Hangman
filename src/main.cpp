a0eccce7f9f9486eafe50ec86fd0cede0267149e
Maximiliano <mfalicoff@yahoo.com>
2020-01-13 20:39:32 -0500

    hide input

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <termios.h>
+#include <unistd.h>
+
 
 using namespace std;
+
+//TODO: Put everything in different files
+//TODO: UI
 
 int main(){
 
 
-    string word;
-    cout << "Enter Word: ", cin >> word; //TODO Dictionary INPUT
+    cout << "Enter Word: "; //TODO Dictionary INPUT
+    system("clear");
 
+    termios oldt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    termios newt = oldt;
+    newt.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    string word;
+    getline(cin, word);
 
     //Initialise blank char array that will be the players current word construction
     char blank[word.size()+1];
@@ -24,10 +38,12 @@ int main(){
     cout << endl;
     bool test = 0;
     int nbGuess = 0;
+
+
     while(!test){
         char guess;
         cout << "Enter guess: ", cin >> guess;
-
+        
         //cout << word.find(guess);
         if(word.find(guess) != string::npos){
             for(int i = 0; i < word.size()+1; i++){
@@ -44,7 +60,7 @@ int main(){
             nbGuess++;
         }
 
-        cout << nbGuess;
+        cout << " " << "number of guesses " << nbGuess << " ";
 
         for (int i = 0; i < word.size(); i++)
         {
@@ -52,7 +68,7 @@ int main(){
             cout << blank[i];
         }
         
-
+        cout << endl;
 
         if(blank == word && nbGuess < 6)
             test = 1;
