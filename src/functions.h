48bdc4cd967bd3ae3dd34c3211fef2feaed4f812
Maximiliano <mfalicoff2001@mail.com>
2020-01-14 09:58:01 -0500

    File structure reform

---
+++ src/functions.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <termios.h>
+#include <unistd.h>
+
+
+bool cmpr(char* guess, std::string word);
+void print();
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <termios.h>
 #include <unistd.h>
+#include "functions.h"
 
 
 using namespace std;
@@ -10,73 +11,81 @@ using namespace std;
 //TODO: Put everything in different files
 //TODO: UI
 
+
 int main(){
 
+    
 
-    cout << "Enter Word: "; //TODO Dictionary INPUT
-    system("clear");
+    //cout << "Enter Word: "; //TODO Dictionary INPUT
+    void print();
+    
+    
 
-    termios oldt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    termios newt = oldt;
-    newt.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    // termios oldt;
+    // tcgetattr(STDIN_FILENO, &oldt);
+    // termios newt = oldt;
+    // newt.c_lflag &= ~ECHO;
+    // tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    string word;
-    getline(cin, word);
-
-    //Initialise blank char array that will be the players current word construction
-    char blank[word.size()+1];
-    for (int i = 0; i < word.size(); i++) {
-        blank[i] = '_';
-    }
-      
+    // string word;
+    // getline(cin, word);
+    // system("clear");
+    // //Initialise blank char array that will be the players current word construction
+    // char blank[word.size()-1];
+    // for (int i = 0; i < word.size(); i++) {
+    //     blank[i] = '_';
+    // }
+    // cout << word.size() <<"\n";
 
     
-    for(int i = 0; i < word.size(); i++)
-        cout << '_';
-    cout << endl;
-    bool test = 0;
-    int nbGuess = 0;
+    // for(int i = 0; i < word.size(); i++)
+    //     cout << '_';
+
+    // cout << endl;
+    // bool test = 0;
+    // int nbWrongGuess = 0;
 
 
-    while(!test){
-        char guess;
-        cout << "Enter guess: ", cin >> guess;
+    // while(!test){
+    //     char guess;
+    //     cout << "Enter guess: ", cin >> guess;
         
-        //cout << word.find(guess);
-        if(word.find(guess) != string::npos){
-            for(int i = 0; i < word.size()+1; i++){
-                if((blank[i] == '_')){
-                    blank[i] = guess;
-                    break;
-                }
+    //     //cout << word.find(guess);
+    //     if(word.find(guess) != string::npos){
+    //         for(int i = 0; i < word.size()+1; i++){
+    //             if((blank[i] == '_')){
+    //                 blank[i] = guess;
+    //                 break;
+    //             }
             
-            }
+    //         }
 
 
-        }
-        else{
-            nbGuess++;
-        }
+    //     }
+    //     else{
+    //         nbWrongGuess++;
+    //     }
 
-        cout << " " << "number of guesses " << nbGuess << " ";
+    //     cout << " " << "number of guesses " << nbWrongGuess << " ";
 
-        for (int i = 0; i < word.size(); i++)
-        {
-            /* code */
-            cout << blank[i];
-        }
+    //     for (int i = 0; i < word.size(); i++)
+    //     {
+    //         /* code */
+    //         cout << blank[i];
+    //     }
         
-        cout << endl;
+    //     cout << endl;
 
-        if(blank == word && nbGuess < 6)
-            test = 1;
-        else if(nbGuess == 6){
-            cout << "You Lose.\n";
-            test = 1;
-        }
+    //     if(nbWrongGuess == 6){
+    //         cout << "You Lose.\n";
+    //         test = 1;
+    //     } else if(cmpr(blank, word)){
+    //         cout << "You win\n"; 
+    //     }
+
             
-    }
-
+         
+    //     }
+            
 }
+
